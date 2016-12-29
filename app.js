8df107263a837850ef7190e1dacca4911bc27544
thatNigerianGuy <otudornyong@gmail.com>
2016-12-28 20:51:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -9,3 +9,21 @@ playlist.add(isreal);
 var playlistElement = document.getElementById('playlist'); 
 
 playlist.renderInElement(playlistElement); 
+
+var playButton = document.getElementById('play'); 
+playButton.onclick = function() { 
+    playlist.play(); 
+    playlist.renderInElement(playlistElement); 
+}
+
+var nextButton = document.getElementById('next'); 
+nextButton.onclick = function() { 
+    playlist.next(); 
+    playlist.renderInElement(playlistElement); 
+}
+var stopButton = document.getElementById('stop'); 
+stopButton.onclick = function() { 
+    playlist.stop(); 
+    playlist.renderInElement(playlistElement); 
+}
+
